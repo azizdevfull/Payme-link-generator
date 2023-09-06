edd6c49254b5d259dc4f2c6e081e2512d3d92e31
Azizbek Isroilov <aziz16110904@gmail.com>
2023-09-06 12:29:19 +0500

    payme link generator

---
+++ payme.php
@@ -0,0 +1,20 @@
+<?php
+
+class PayComResponse {
+    const LINK = 'https://checkout.paycom.uz';
+
+    public static function createInitialization() {
+        $TOKEN = '64ef2f2d9f1e571160d52f76';
+        $order_id = '1';
+        $amount = 5000; // Amount must be decimal
+
+        $params = "m={$TOKEN};ac.order_id={$order_id};a={$amount};"; // Use $amount_decimal
+        $encode_params = base64_encode(utf8_encode($params));
+        $url = self::LINK . '/' . $encode_params;
+        echo $url;
+    }
+}
+
+// Example usage:
+PayComResponse::createInitialization();
+?>
